c423cbbbd6ac3d2bd1a3dded87e486b18052943e
asultan123 <asultan@aucegypt.edu>
2020-10-08 01:07:07 +0000

    Added basic skelenton for dma_socket_wrapper

--- systemc/dma_socket_wrapper.cc
+++ systemc/dma_socket_wrapper.cc
@@ -6,14 +6,9 @@
 #include "tlm_utils/simple_target_socket.h"
 
 
-using namespace std;
-using namespace sc_score;
-
-
-
 #include "dma_socket_wrapper.h"
 
-dma_socket_wrapper::dma_socket_wrapper(sc_module_name name) : sc_module(name)
+dma_socket_wrapper::dma_socket_wrapper(sc_module_name name, int _size) : sc_module(name)
 
 {
 
@@ -22,6 +17,63 @@ dma_socket_wrapper::dma_socket_wrapper(sc_module_name name) : sc_module(name)
   socket.register_b_transport(this, &dma_socket_wrapper::b_transport);
   socket.register_get_direct_mem_ptr(this, &dma_socket_wrapper::get_direct_mem_ptr); 
 
-  //TODO: Init data buffer 
+
+  size = _size;
+  data_buffer = new uint8_t[size]; // This buffer will be used to store descriptor from user-app
+  memset(&data_buffer[0], 0, size);
+
 
 }
+
+
+
+//TODO: in b_transport, I need to check if dma_ptr == NULL
+
+void dma_socket_wrapper::b_transport(tlm::tlm_generic_payload& trans, sc_time& delay)
+{
+	tlm::tlm_command cmd = trans.get_command();
+	sc_dt::uint64    addr = trans.get_address();
+	unsigned char*   ptr = trans.get_data_ptr();
+	unsigned int     len = trans.get_data_length();
+	unsigned char*   byt = trans.get_byte_enable_ptr();
+
+	if (addr > sc_dt::uint64(size)) {
+		trans.set_response_status(tlm::TLM_ADDRESS_ERROR_RESPONSE);
+		SC_REPORT_FATAL("Memory", "Unsupported access\n");
+		return;
+	}
+	if (byt != 0) {
+		trans.set_response_status(tlm::TLM_BYTE_ENABLE_ERROR_RESPONSE);
+		SC_REPORT_FATAL("Memory", "Unsupported access\n");
+		return;
+	}
+
+	if (trans.get_command() == tlm::TLM_READ_COMMAND){
+  
+  }
+	else if (cmd == tlm::TLM_WRITE_COMMAND){
+
+  }
+
+	delay += LATENCY;
+
+	trans.set_dmi_allowed(true);
+	trans.set_response_status(tlm::TLM_OK_RESPONSE);
+}
+
+
+bool dma_socket_wrapper::get_direct_mem_ptr(tlm::tlm_generic_payload& trans,
+				tlm::tlm_dmi& dmi_data)
+{
+	dmi_data.allow_read_write();
+
+	dmi_data.set_dmi_ptr( reinterpret_cast<unsigned char*>(&data_buffer[0]));
+	dmi_data.set_start_address(0);
+	dmi_data.set_end_address(size - 1);
+	/* Latencies are per byte.  Our latency is expressed per access,
+	   which are in 32bits so dividie by 4. Is there a better way?.  */
+	dmi_data.set_read_latency(LATENCY / 4);
+	dmi_data.set_write_latency(LATENCY / 4);
+	return true;
+}
+
--- systemc/dma_socket_wrapper.h
+++ systemc/dma_socket_wrapper.h
@@ -1,15 +1,21 @@
-class dma_socket_wrapper
-: public sc_score::sc_module 
-{
-public:
-  tlm_utils::simple_target_socket<dma_socket_wrapper> socket;
+#include "DMA.cc"
 
-  dma_socket_wrapper(sc_core::sc_module_name name);
+class dma_socket_wrapper
+: public sc_core::sc_module 
+{
+  public:
+  tlm_utils::simple_target_socket<dma_socket_wrapper> socket;
+  DMA *dma_ptr = NULL;
+  const sc_time LATENCY;
+
+  dma_socket_wrapper(sc_core::sc_module_name name, int _size);
   virtual void b_transport(tlm::tlm_generic_payload& trans,
                 sc_time& delay);
   virtual bool get_direct_mem_ptr(tlm::tlm_generic_payload& trans,
                                           tlm::tlm_dmi& dmi_data);
-
+  private:
+  int size;
+  uint8_t *data_buffer;
 
 
 };
